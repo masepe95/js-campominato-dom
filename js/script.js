9f9cf08e4e7ed0b549c071bfadbd4969f14f63e4
masepe95 <marcosepe10@gmail.com>
2023-06-08 23:29:42 +0200

    generate bombs

--- js/script.js
+++ js/script.js
@@ -6,17 +6,37 @@ console.log('JS OK')
 const gridField = document.getElementById('grid');
 const playButton = document.querySelector('button');
 const difficultySelector = document.getElementById('difficulty');
+const scoreField = document.getElementById('score');
 
 // Dichiaro dati griglia
 const hardGame = 49;
 const mediumGame= 81;
 const easyGame = 100;
+let score = 0;
+const totalBombs = 16;
+const maxScore = easyGame - totalBombs;
 
+const bombs = [];
+
+function generateBombs(totalBombs, easyGame){
+let bombs = [];
+
+while (bombs.length < totalBombs) {
+    let randomNumber;
+    do {
+        randomNumber = Math.floor(Math.random() * easyGame) + 1;
+    } while (bombs.includes(randomNumber));
+    bombs.push(randomNumber);
+}
+
+return bombs;
+
+}
 
 //funzione arrow per creare cell
 const createCell = () => {
     const cell = document.createElement('div');
-    cell.className = 'cellEasy';
+    cell.className = 'cell';
 
     return cell;
 }
@@ -27,14 +47,26 @@ playButton.addEventListener('click', function() {
             const cell = createCell();
 
             cell.innerText = i + 1;
-            
-            cell.addEventListener('click', () => {
-                cell.classList.toggle('clicked');
-                console.log(i + 1);
-            });
 
-            gridField.appendChild(cell);
+        cell.addEventListener('click', () => {
+            
+            if (cell.classList.contains('clicked')) return;
+
+            scoreField.innerText = ++score;
+            
+            cell.classList.add('clicked');
+            console.log(cell.innerText);
+            
+        });
+
+        gridField.appendChild(cell);
         }
+
     })
+
+
+
+
+
 
 
